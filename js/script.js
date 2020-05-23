e81f267f9de19f7d2be3a2744126e9b7acc77c04
Lev Misyuk <lev.misyuk01@gmail.com>
2020-05-24 02:33:57 +0300

    Basic HTML/CSS template with a bit of JS animation. TODO: - Figure out what's wrong with the button placement

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.center h1', {
+    strings: ['Забронируйте номер прямо сейчас', 'Проведите лучший отпуск в нашем отеле'],
+    typeSpeed: 30,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false
+})
